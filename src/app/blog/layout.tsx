56518428cc26205b3f6696ae92416e99c3c9973a
Harshita Beohar <harshitabeohar@Harshitas-MacBook-Air.local>
2025-03-07 12:36:10 +0000

    fixed tailwind

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -6,11 +6,11 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div style={{ display: "flex" }}>
-      <div style={{ width: "10%" }}>
-        <SideNav />
-      </div>
-      <div>{children}</div>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-white">
+    <div className="w-full flex-none md:w-64">
+      <SideNav />
     </div>
+    <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+  </div>
   );
 }
