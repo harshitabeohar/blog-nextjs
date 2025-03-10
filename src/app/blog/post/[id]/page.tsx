245d0f9ff519a8f9421c50dd64a825c1081ba354
Harshita Beohar <harshitabeohar@Harshitas-MacBook-Air.local>
2025-03-10 14:55:20 +0000

    fixes

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -3,9 +3,10 @@ import { notFound } from 'next/navigation';
 import Post from '@/app/ui/components/posts/Post';
 import { getPosts } from '@/app/lib/data';
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await params;
   const posts = await getPosts();
-  const post = posts?.find((post) => post.id === params.id);
+  const post = posts?.find((post) => post.id === resolvedParams.id);
 
   if (!post) {
     return notFound();
